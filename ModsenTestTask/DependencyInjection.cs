36a3df5b2e8e51bc85c1cf5d408f80ac399a9748
pahdik <92573772+pahdik@users.noreply.github.com>
2023-04-06 23:17:07 +0300

    Created README

--- ModsenTestTask/DependencyInjection.cs
+++ ModsenTestTask/DependencyInjection.cs
@@ -23,43 +23,6 @@ namespace Library.WebApi
 {
     public static class DependencyInjection
     {
-        //public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
-        //{
-        //    services.AddDbContext<DataContext>(options =>
-        //    options.UseSqlServer(
-        //             configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("Library.DAL")));
-
-        //    services.AddScoped<IApplicationContext, ApplicationContext>();
-
-        //    services.AddIdentityCore<User>().AddEntityFrameworkStores<ApplicationContext>().AddSignInManager<SignInManager<User>>();
-
-        //    services.AddAuth(configuration);
-        //    services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
-
-        //    return services;
-        //}
-
-        //public static IServiceCollection AddAuth(this IServiceCollection services, ConfigurationManager configuration)
-        //{
-        //    var jwtSettings = new JwtSettings();
-        //    configuration.Bind(JwtSettings.SectionName, jwtSettings);
-
-        //    services.AddSingleton(Options.Create(jwtSettings));
-        //    services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
-
-        //    services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
-        //        .AddJwtBearer(options => options.TokenValidationParameters = new()
-        //        {
-        //            ValidateIssuer = true,
-        //            ValidateAudience = true,
-        //            ValidateLifetime = true,
-        //            ValidateIssuerSigningKey = true,
-        //            ValidIssuer = jwtSettings.Issuer,
-        //            ValidAudience = jwtSettings.Audience,
-        //            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret))
-        //        });
-
-        //    return services;
-        //}
+        
     }
 }
---
+++ README.md
@@ -0,0 +1,9 @@
+# Run application
+### PowerShell
+Run PowerShell inside ./ModsenTestTask folder and enter the following command: `dotnet run --project ModsenTestTask`
+
+# Update database
+### Visual Studio
+- change the connection string which is in ./ModsenTestTask/ModsenTestTask/appsettings.json
+- Open "Package Manager Console"(View -> Other Windows -> Package Manager Console)
+- Enter command: `Update-Database`
